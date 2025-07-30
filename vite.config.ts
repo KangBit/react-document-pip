738966313d92e18ccaabb4b8c704f23a15f3295b
Kangbit <kangbit@smartscore.kr>
2025-07-30 10:53:21 +0900

    fix: reduce bundle size by excluding JSX runtime from bundle

--- vite.config.ts
+++ vite.config.ts
@@ -24,11 +24,18 @@ export default defineConfig({
       fileName: "index",
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: [
+        "react",
+        "react-dom",
+        "react/jsx-runtime",
+        "react/jsx-dev-runtime",
+      ],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
+          "react/jsx-runtime": "React.jsxRuntime",
+          "react/jsx-dev-runtime": "React.jsxDevRuntime",
         },
       },
     },
